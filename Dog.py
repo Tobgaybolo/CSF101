3df5d3bece9aa761d3d8fe268120f6d29f40516e
Tobgaybolo <141105504+Tobgaybolo@users.noreply.github.com>
2024-10-11 15:42:50 +0600

    Completed cap1

--- Dog.py
+++ Dog.py
@@ -1,15 +1,22 @@
-class Dog:
-    def __init__(self, name, color, Age):
-        self.name = name
-        self.color = color
-        self.Age = Age
+# class Dog:
+#     def __init__(self, name, color, Age):
+#         self.name = name
+#         self.color = color
+#         self.Age = Age
 
-    def bark(self):
-        return f" {self.name} {self.color} {self.Age} bark"
+#     def bark(self):
+#         return f" {self.name} {self.color} {self.Age} bark"
     
-Nima = Dog("Nima", "yellow", "5")
-Dawa = Dog("Dawa", "brown", "7")
-Bruno = Dog("Bruno", "black", "4")
+# Nima = Dog("Nima", "yellow", "5")
+# Dawa = Dog("Dawa", "brown", "7")
+# Bruno = Dog("Bruno", "black", "4")
 
-print(Bruno.bark())
-        
+# print(Bruno.bark())
+
+
+class Student:
+    def __init__(self):
+        self.__id = 123
+
+Sonam = Student()
+print(Sonam.__id)
